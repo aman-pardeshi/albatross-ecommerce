b7199a42549b92f76d9df8f031e67385f6f2a19b
MayankG <mayankgupta.work@gmail.com>
2022-10-13 23:25:10 +0530

    response added and refactored

--- backend/controlers/orderControler.js
+++ backend/controlers/orderControler.js
@@ -15,24 +15,35 @@ const createNewOrder = asyncHandler(async (req, res) => {
     totalPrice,
   } = req.body;
 
-  if (orderItems && orderItems.length === 0) {
-    res.status(400);
-    throw new Error('No order items');
-  } else {
-    const order = new Order({
-      orderItems,
-      shippingAddress,
-      paymentMethod,
-      itemsPrice,
-      taxPrice,
-      shippingPrice,
-      totalPrice,
-      user: req.user._id,
-    });
+  orderItems.length = 0
 
-    const createdOrder = await order.save();
-    res.status(201).json(createdOrder);
-  }
+  if (Array.isArray(orderItems) && !orderItems.length) {
+    return res.status(400).json({
+      success: false,
+      msg: 'Order items empty',
+      data: null
+    });
+  } 
+  
+  const order = new Order({
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    user: req.user._id,
+  });
+
+  const createdOrder = await order.save();
+
+  return res.status(201).json({
+    success: true,
+    msg: 'Order found',
+    data: createdOrder
+  });
+
 });
 
 // @Description: Get order by ID
